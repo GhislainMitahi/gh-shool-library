5c8f0588705a1ec98bf8f81aa8660c4e1f832f0d
ghislainmitahi@gmail.com <ghislainmitahi@gmail.com>
2022-11-12 05:48:25 +0200

    update student once

--- student.rb
+++ student.rb
@@ -1,17 +1,9 @@
 require './person'
-require './classroom'
 
 class Student < Person
-  attr_reader :classroom
-
-  def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
-    super(name: name, age: age, parent_permission: parent_permission)
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
     @classroom = classroom
-  end
-
-  def classroom=(classroom)
-    @classroom = classroom
-    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
